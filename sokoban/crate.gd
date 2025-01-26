c36dbe78c8cfb7dbb67d1a551e1919ee474ad5c5
Gabi <gabriel.s.racz@gmail.com>
2025-01-26 18:03:14 -0500

    use box query instead of ray to detect crate on crate violence

--- sokoban/crate.gd
+++ sokoban/crate.gd
@@ -25,35 +25,28 @@ func _process(delta: float) -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	var space_state = get_world_3d().direct_space_state
-	
-	#Set up the ray query parameters
-	if(linear_velocity.length() > 0.001):
-		var ray_query = PhysicsRayQueryParameters3D.new()
-		ray_query.from = global_position
-		ray_query.to = global_position + linear_velocity.normalized() * 0.55
-		ray_query.collision_mask = collision_mask
-		# Perform the raycast
-		var result = space_state.intersect_ray(ray_query)
-		if result:
-			if result["collider"].is_class("RigidBody3D"):
-				result["collider"].freeze = true
-				linear_velocity = Vector3.ZERO
-				return
-
 	global_transform = Transform3D(
 		original_transform.basis,  # Keep the original rotation
 		global_transform.origin    # Allow position to change
 	)
 	angular_velocity = Vector3.ZERO
 	linear_velocity = Vector3(linear_velocity.x, 0, linear_velocity.z)
+	
+	# Ensure no crates will be pushed by this one
+	var space_state = get_world_3d().direct_space_state
+	if(linear_velocity.length() > 0.001):
+		var box_shape = BoxShape3D.new()
+		box_shape.size = Vector3(0.9, 0.9, 0.9)
 
-func _integrate_forces(state: PhysicsDirectBodyState3D):
-	return
-	for contact in state.get_contact_count():
-		var other_body = state.get_contact_collider_object(contact)
-		if other_body is RigidBody3D:
-			# Suppress velocity to simulate static behavior
-			state.linear_velocity = Vector3.ZERO
-			state.angular_velocity = Vector3.ZERO
-			return  # Exit after processing one contact to avoid unnecessary checks
+		var shape_query = PhysicsShapeQueryParameters3D.new()
+		shape_query.shape = box_shape
+		shape_query.transform = Transform3D(Basis(), global_position + (linear_velocity.normalized() * 0.05))
+		shape_query.collision_mask = collision_mask
+
+		var results = space_state.intersect_shape(shape_query, 10)
+		if results.size() > 0:
+			for result in results:
+				if result.collider is Crate and result.collider.get_instance_id() != get_instance_id():
+					result.collider.freeze = true
+					linear_velocity = Vector3.ZERO
+					return
